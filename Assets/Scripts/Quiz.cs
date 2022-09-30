f00ad8d53141d93b5a683a04910619d0a013698d
silvia <silviapintus.93@gmail.com>
2022-09-30 15:44:03 +0200

    change color of buttons if answer is correct/wrong

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
 	[SerializeField] TextMeshProUGUI questionText;
 	[SerializeField] QuestionSO question;
 	[SerializeField] GameObject[] answerButtons;
+	int correctAnswerIndex;
+	[SerializeField] Color32 defaultAnswerColor = new Color32 (1, 1, 1, 1);
+	[SerializeField] Color32 correctAnswerColor = new Color32 (1, 1, 1, 1);
+	[SerializeField] Color32 wrongAnswerColor = new Color32 (1, 1, 1, 1);
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -16,6 +22,21 @@ public class Quiz : MonoBehaviour
 		for(int i = 0; i < answerButtons.Length; i++){
 			TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
 		  buttonText.text = question.GetAnswer(i);
+		}
+	}
+
+	public void OnAnswerSelected(int index)
+	{
+		if(index == question.GetCorrectAnswerIndex()){
+			questionText.text = "Correct!";
+			Image buttonImage = answerButtons[index].GetComponent<Image>();
+			buttonImage.color = correctAnswerColor;
+		} 
+		else
+		{
+			questionText.text = "Wrong!";
+			Image buttonImage = answerButtons[index].GetComponent<Image>();
+			buttonImage.color = wrongAnswerColor;
 		}
 	}
 }
